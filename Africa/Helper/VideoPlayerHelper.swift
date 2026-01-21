d0801695f11c54f167d5623599023c0a5a499012
Labhesh <labhesh.dudi@gmail.com>
2026-01-21 14:09:48 +0530

    video player

---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Labhesh Dudi on 21/01/26.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if let url = Bundle.main.url(forResource: fileName, withExtension: fileFormat) {
+        videoPlayer = AVPlayer(url: url)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
