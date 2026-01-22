195b49107a2e318ecc690753973c6b0a5bc573f7
Labhesh <labhesh.dudi@gmail.com>
2026-01-22 14:59:21 +0530

    map part 1

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,13 +6,50 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    // MARK: - PROPERTIES
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinate = CLLocationCoordinate2D(latitude: 6.6, longitude: 16.43)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinate, span: mapZoomLevel)
+        
+        return mapRegion
+    }()
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Map")
+        // MARK: - No1 Basic MAP
+        // Map(coordinateRegion: $region)
+        
+        // MARK: - No2 AdVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // (A) PIN: OLD STYLE (always static)
+//            MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // (B) MARKER: NEW STYLE (always static)
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // (C) CUSTOM BASIC ANNOTATION (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32, alignment: .center)
+                
+            }
+        })
     }
 }
 
+// MARK: - PREVIEW
+
 #Preview {
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    
     MapView()
 }
---
+++ Africa/View/MapAnnotationView.swift
@@ -0,0 +1,50 @@
+//
+//  MapAnnotationView.swift
+//  Africa
+//
+//  Created by Labhesh Dudi on 22/01/26.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    
+    // MARK: - PROPERTIES
+    
+    var location: NationalParkLocation
+    @State private var animation: Double = 0.0
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.accentColor)
+                .frame(width:54, height: 54, alignment: .center)
+            
+            Circle()
+                .stroke(Color.accentColor, lineWidth: 2)
+                .frame(width: 52, height: 52, alignment: .center)
+                .scaleEffect(1 + CGFloat(animation))
+                .opacity(1 - animation)
+            
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48, alignment: .center)
+                .clipShape(Circle())
+        }//: ZSTACK
+        .onAppear {
+            withAnimation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false)) {
+                animation = 1
+            }
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    MapAnnotationView(location: locations[1])
+}
